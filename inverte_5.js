fa70bc820e3cc0c11e166933dfe3021f1df68df6
Barbara Luz <barbaraluz@eq.ufrj.br>
2023-04-06 13:41:49 -0300

    fix exercicio 5

--- inverte_5.js
+++ inverte_5.js
@@ -1,5 +1,15 @@
 function invert(string) {
-    return string.split('').reverse().join('')
+    let str = string.split('')
+
+    let str_rev = []
+
+    for (let i=str.length-1; i>=0; i--) {
+        str_rev.push(str[i])
+    }
+
+    let string_reverse = str_rev.join('')
+
+    return string_reverse
 }
 
 console.log(invert('oi'))
